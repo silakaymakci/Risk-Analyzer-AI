4dc3b422b338c948e5e2bbb3e38c46cb8e6c320b
silakaymakci <silakaymakci67@gmail.com>
2026-03-19 17:27:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
+import streamlit as st
 import yfinance as yf
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+st.title("Chaos & Coherence: Dinamik Risk Analizörü")
+
 # 1. Veri Çekme
-hisseler=["THYAO.IS", "EREGL.IS", "SISE.IS", "BTC-USD", "GC=f"] 
-# Türk hisseleri, Bitcoin ve Altın
-data=yf.download(hisseler, start="2023-01-01")['Close']
+hisseler = ["THYAO.IS", "EREGL.IS", "SISE.IS", "BTC-USD", "GC=F"]
+data = yf.download(hisseler, start="2023-01-01")['Close']
 
 # 2. Günlük Getiri ve Korelasyon (Matematik kısmı)
-returns=data.pct_change()
-corr=returns.corr()
+returns = data.pct_change()
+corr = returns.corr()
 
-# 3.Görselleştirme
-sns.heatmap(corr, annot=True, cmap='coolwarm')
-plt.show()
+# 3. Görselleştirme
+fig, ax = plt.subplots()
+sns.heatmap(corr, annot=True, cmap='coolwarm', ax=ax)
+st.pyplot(fig)
+
+st.write("Matematikçinin Notu: Portföyünüzdeki varlıkların birbiriyle olan ilişkisini yukarıdaki tabloda görebilirsiniz.")
