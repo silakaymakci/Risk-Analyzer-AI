e0d273f01acac99b7c21ffbd4d6994d5e82b8eee
silakaymakci <silakaymakci67@gmail.com>
2026-03-19 17:18:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # 1. Veri Çekme
 hisseler=["THYAO.IS", "EREGL.IS", "SISE.IS", "BTC-USD", "GC=f"] 
 # Türk hisseleri, Bitcoin ve Altın
-data=yf.download(hisseler,, start="2023-01-01")['Close']
+data=yf.download(hisseler, start="2023-01-01")['Close']
 
 # 2. Günlük Getiri ve Korelasyon (Matematik kısmı)
 returns=data.pct_chance()
